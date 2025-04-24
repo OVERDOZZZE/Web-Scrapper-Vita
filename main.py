0df2d4c2d763e65df1b3afa047852118973178dc
OVERDOZZZE <saparbekov113@gmail.com>
2025-04-23 22:25:58 -0500

    Spinner is back

--- main.py
+++ main.py
@@ -15,15 +15,15 @@ class App:
         self.writer = ExcelWriter(self.settings.PATH)
 
     def run(self):
-        # stop_event = threading.Event()
-        # spinner = Spinner(stop_event)
-        # spinner_thread = threading.Thread(target=spinner.start)
-        # spinner_thread.start()
+        stop_event = threading.Event()
+        spinner = Spinner(stop_event)
+        spinner_thread = threading.Thread(target=spinner.start)
+        spinner_thread.start()
 
         self.writer.write(self.scrapper.scrapper())
-        #
-        # stop_event.set()
-        # spinner_thread.join()
+        
+        stop_event.set()
+        spinner_thread.join()
 
 
 if __name__ == '__main__':
